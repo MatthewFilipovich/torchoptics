1f34a5e0dddc6dfa0f4835b5c8b709b66e7cbfdc
Matthew Filipovich <42307495+MatthewFilipovich@users.noreply.github.com>
2025-01-15 17:24:50 +0000

    Update symbol formatting in IntensityDetector visualization

--- torchoptics/elements/detectors.py
+++ torchoptics/elements/detectors.py
@@ -114,7 +114,7 @@ class IntensityDetector(Element):
             sum_weight (bool): Whether to plot the sum of the weight matrix. Default: `False`.
             **kwargs: Additional keyword arguments for visualization.
         """
-        kwargs.update({"symbol": "$W$"})
+        kwargs.update({"symbol": r"$\mathcal{W}" + f"_{index[-1]}$"})
         return self._visualize(self.weight, index, **kwargs)
 
     @staticmethod
