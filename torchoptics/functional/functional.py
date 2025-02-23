eb9aa8dcad33277b598493350dea92a31caf6ee8
Matthew Filipovich <42307495+MatthewFilipovich@users.noreply.github.com>
2025-02-23 13:01:52 +0000

    Remove unused import

--- torchoptics/functional/functional.py
+++ torchoptics/functional/functional.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Sequence, Union
+from typing import TYPE_CHECKING, Sequence, Union
 
 import torch
 from torch import Tensor
