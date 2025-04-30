6e0edea44aa6dd3a2ad6c78a43271deab55b3fd2
Matthew Filipovich <42307495+MatthewFilipovich@users.noreply.github.com>
2025-04-30 14:02:09 +0100

    Refactor animations example

--- docs/source/examples/animations.py
+++ docs/source/examples/animations.py
@@ -5,30 +5,30 @@ Animations
 Demonstrates how to create animations of field propagation in TorchOptics.
 """
 
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
 import torch
 
 import torchoptics
-from torchoptics import Field, animate_tensor
+from torchoptics import Field
 from torchoptics.profiles import circle
+from torchoptics.visualization import animate_tensor
 
 # Set default simulation parameters
 torchoptics.set_default_spacing(10e-6)
 torchoptics.set_default_wavelength(700e-9)
 
-# Initialize a 1D field
+# Initialize field with a circular profile
 field = Field(circle(200, radius=50e-5))
 field.visualize(vmax=2)
 
 # %%
-# Propagate
+# Propagate the field
 
 z_values = torch.linspace(0, 0.5, 201)
 intensities = torch.stack([field.propagate_to_z(z).intensity() for z in z_values])
 
 # %%
 # Animate
+
 vmax = 3
 titles = [f"z = {z:.2f} m" for z in z_values]
 func_anim_kwargs = {"interval": 100}
